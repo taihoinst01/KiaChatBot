0d528f6b4b73cf2b907b11d0022d4765b6848c0a
songhj <songhj@SONGHJ-PC>
2017-09-12 17:21:22 +0900

    welcom message update

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -5,6 +5,13 @@ using Microsoft.Bot.Connector;
 using Microsoft.Bot.Builder.Dialogs;
 using System.Web.Http.Description;
 using System.Net.Http;
+using System;
+using System.Linq;
+using SimpleEchoBot.DB;
+using System.Collections.Generic;
+using SimpleEchoBot.Models;
+using System.Diagnostics;
+using System.Net;
 
 namespace Microsoft.Bot.Sample.SimpleEchoBot
 {
@@ -19,17 +26,175 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
         [ResponseType(typeof(void))]
         public virtual async Task<HttpResponseMessage> Post([FromBody] Activity activity)
         {
-            // check if activity is of type message
-            if (activity != null && activity.GetActivityType() == ActivityTypes.Message)
+
+            HttpResponseMessage response;
+
+            // welcome message 출력   
+            if (activity.Type == ActivityTypes.ConversationUpdate && activity.MembersAdded.Any(m => m.Id == activity.Recipient.Id))
             {
-                //test
-                await Conversation.SendAsync(activity, () => new EchoDialog());
+                DateTime startTime = DateTime.Now;
+
+                // Db
+                DbConnect db = new DbConnect();
+                List<DialogList> dlg = db.SelectInitDialog();
+                Debug.WriteLine("!!!!!!!!!!! : " + dlg[0].dlgId);
+
+                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+
+                for (int n = 0; n < dlg.Count; n++)
+                {
+
+                    Activity reply2 = activity.CreateReply();
+                    reply2.Recipient = activity.From;
+                    reply2.Type = "message";
+                    reply2.Attachments = new List<Attachment>();
+                    reply2.AttachmentLayout = AttachmentLayoutTypes.Carousel;
+
+                    List<CardList> card = db.SelectDialogCard(dlg[n].dlgId);
+                    List<TextList> text = db.SelectDialogText(dlg[n].dlgId);
+                    List<MediaList> media = db.SelectDialogMedia(dlg[n].dlgId);
+
+                    for(int i = 0; i < text.Count; i++)
+                    {
+                        HeroCard plCard = new HeroCard()
+                        {
+                            Title = text[i].cardTitle,
+                            Subtitle = text[i].cardText
+                        };
+
+                        Attachment plAttachment = plCard.ToAttachment();
+                        reply2.Attachments.Add(plAttachment);
+                    }
+
+                    for(int i = 0; i < card.Count; i++)
+                    {
+                        List<CardImage> cardImages = new List<CardImage>();
+                        List<CardAction> cardButtons = new List<CardAction>();
+
+                        if (card[i].imgUrl != null)
+                        {   
+                            cardImages.Add(new CardImage(url: card[i].imgUrl));
+                        }
+
+                        if(card[i].btn1Type != null)
+                        {
+                            CardAction plButton = new CardAction()
+                            {
+                                Value = card[i].btn1Context,
+                                Type = card[i].btn1Type,
+                                Title = card[i].btn1Title
+                            };
+
+                            cardButtons.Add(plButton);
+                        }
+
+                        if (card[i].btn2Type != null)
+                        {
+                            CardAction plButton = new CardAction()
+                            {
+                                Value = card[i].btn2Context,
+                                Type = card[i].btn2Type,
+                                Title = card[i].btn2Title
+                            };
+
+                            cardButtons.Add(plButton);
+                        }
+
+                        if (card[i].btn3Type != null)
+                        {
+                            CardAction plButton = new CardAction()
+                            {
+                                Value = card[i].btn3Context,
+                                Type = card[i].btn3Type,
+                                Title = card[i].btn3Title
+                            };
+
+                            cardButtons.Add(plButton);
+                        }
+
+                        HeroCard plCard = new HeroCard()
+                        {
+                            Title = card[i].cardTitle,
+                            Subtitle = card[i].cardSubTitle,
+                            Images = cardImages,
+                            Buttons = cardButtons
+                        };
+
+                        Attachment plAttachment = plCard.ToAttachment();
+                        reply2.Attachments.Add(plAttachment);
+                    }
+
+                    for(int i = 0; i < media.Count; i++)
+                    {
+                        List<MediaUrl> mediaURL = new List<MediaUrl>();
+                        List<CardAction> cardButtons = new List<CardAction>();
+
+                        if (media[i].mediaUrl != null)
+                        {
+                            mediaURL.Add(new MediaUrl(url: media[i].mediaUrl));
+                        }
+
+                        if (media[i].btn1Type != null)
+                        {
+                            CardAction plButton = new CardAction()
+                            {
+                                Value = media[i].btn1Context,
+                                Type = media[i].btn1Type,
+                                Title = media[i].btn1Title
+                            };
+
+                            cardButtons.Add(plButton);
+                        }
+
+                        if (media[i].btn2Type != null)
+                        {
+                            CardAction plButton = new CardAction()
+                            {
+                                Value = media[i].btn2Context,
+                                Type = media[i].btn2Type,
+                                Title = media[i].btn2Title
+                            };
+
+                            cardButtons.Add(plButton);
+                        }
+
+                        if (media[i].btn3Type != null)
+                        {
+                            CardAction plButton = new CardAction()
+                            {
+                                Value = media[i].btn3Context,
+                                Type = media[i].btn3Type,
+                                Title = media[i].btn3Title
+                            };
+
+                            cardButtons.Add(plButton);
+                        }
+
+                        VideoCard plCard = new VideoCard()
+                        {
+                            Title = media[i].cardTitle,
+                            Text = media[i].cardText,
+                            Media = mediaURL,
+                            Buttons = cardButtons,
+                            Autostart = false
+                        };
+
+                        Attachment plAttachment = plCard.ToAttachment();
+                        reply2.Attachments.Add(plAttachment);
+                    }
+
+                    var reply1 = await connector.Conversations.SendToConversationAsync(reply2);
+                }
+                DateTime endTime = DateTime.Now;
+                Debug.WriteLine("프로그램 수행시간 : {0}/ms", ((endTime - startTime).Milliseconds));
             }
-            else
+            else if (activity.Type == ActivityTypes.Message)
             {
                 HandleSystemMessage(activity);
             }
-            return new HttpResponseMessage(System.Net.HttpStatusCode.Accepted);
+
+            response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
         }
 
         private Activity HandleSystemMessage(Activity message)
---
+++ Models/DialogList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleEchoBot.Models
+{
+    public class DialogList
+    {
+        public int dlgId;
+        public string dlgNm;
+        public string dlgMent;
+        public string dlgLang;
+    }
+}
