d7df24814c3288c80352ef8c2012e1801ad8b4e3
songhj <songhj@SONGHJ-PC>
2017-09-21 14:01:04 +0900

    commit

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -208,11 +208,11 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
 
                 orgMent = activity.Text;
 
-                Translator translateInfo = await getTranslate(orgMent);
-                orgENGMent_history = Regex.Replace(translateInfo.data.translations[0].translatedText, @"[^a-zA-Z0-9¤¡-ÆR-\s-&#39;]", "", RegexOptions.Singleline);
-                orgENGMent_history = orgENGMent_history.Replace("&#39;", "'");
+                //Translator translateInfo = await getTranslate(orgMent);
+                //orgENGMent_history = Regex.Replace(translateInfo.data.translations[0].translatedText, @"[^a-zA-Z0-9¤¡-ÆR-\s-&#39;]", "", RegexOptions.Singleline);
+                //orgENGMent_history = orgENGMent_history.Replace("&#39;", "'");
 
-                Luis = await GetIntentFromBotLUIS(orgENGMent_history);
+                Luis = await GetIntentFromBotLUIS(orgMent);
 
                 Debug.WriteLine("score : " + (float)Luis["intents"][0]["score"]);
                 Debug.WriteLine("score : " + Luis["entities"].Count());
